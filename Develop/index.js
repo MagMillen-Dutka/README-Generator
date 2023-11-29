b7ae84ef1399c12bf6906613f69c8c1f1e795550
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2023-11-29 17:20:45 +0000

    initial code dump for index.js & generatemarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const generateMarkdown = require ('./utils/generateMarkdown.js');
+const route = require('route');
+const { default: inquirer } = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Please describe this project.',
+        name: 'description',
+      },
+      {
+        type: 'checkbox',
+        message: 'Please select license type.',
+        name: 'license',
+        choices: ["MIT License", "APACHE License 2.0", "GNU General Public License v3.0", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public Lincense v2.1", "Mozilla Public License 2.0", "The Unilicense" ],
+      },
+      {
+        type: 'input',
+        message: 'Please describe how to install this project.',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Please describe the usage.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please provide the test instructions.',
+        name: 'testing',
+      },
+      {
+        type: 'input',
+        message: 'Please describe any notable contributions and collaborations.',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'Please contact me if you have any questions about this README Generator.',
+        name: 'questions',
+      },
+      {
+        type: 'input',
+        message: 'Please write your Github ID.',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'Please your email address.',
+        name: 'email',
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),filename),data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Your professional README.md");
+        writeToFile('./Develop/README', generateMarkdown({responses}));
+    });
+}
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == "none") {
+    return '![Github license](https://img.shields.io/badges/license-${license}-yellowgreen.svg)';
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "none")
+  {
+    return  `\n* [License](#license)\n`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "none") {
+    return `## License
+    
+      Licenses within ${license} licens.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+ 
+  ${renderLicenseBadge(data.license)}
+  
+  ## Deployed Application URL
+  ${data.link}
+  ## Table of Contents
+  * [Description](#description)
+  * [How to Use This Application](#HowtoUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  
+  ## Description
+  ${data.description}
+  ## How to Use This Application:
+  ${data.usage}
+  ## Contributors
+  ${data.contribution}
+  ## Testing
+  ${data.testing}
+  ## Questions
+  Please get in touch via email - ${data.email} or visit [github/${data.username}](https://github.com/${data.username}) if you have any questions.
+  `;
+  }
 `;
 }
 
